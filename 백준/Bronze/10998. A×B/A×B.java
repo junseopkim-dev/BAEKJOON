b281aae560b0b10bd8e8f4ba3fff1834665ab642
Junseop Kim <125976420+junseopkim-dev@users.noreply.github.com>
2023-09-12 12:15:19 +0900

    [Bronze V] Title: A×B, Time: 212 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        int a = stdIn.nextInt();
+        int b = stdIn.nextInt();
+        System.out.println(a*b);
+    }
+}
