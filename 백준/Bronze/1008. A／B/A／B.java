ffc98afd6006d17e0ddf4f03ec3c9b15c8ade828
Junseop Kim <125976420+junseopkim-dev@users.noreply.github.com>
2023-09-12 12:21:24 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        double a = stdIn.nextDouble();
+        double b = stdIn.nextDouble();
+        System.out.println(a/b);
+    }
+}
