8f36c163e0c3627877ea00e0cf33c9c3adf6841f
Junseop Kim <125976420+junseopkim-dev@users.noreply.github.com>
2023-09-13 03:11:24 +0900

    [Bronze V] Title: 개수 세기, Time: 140 ms, Memory: 15936 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,4 +1,6 @@
 //Scanner
+
+/*
 import java.util.Scanner;
 
 public class Main {
@@ -22,12 +24,45 @@ public class Main {
     }
 }
 
-
+*/
 
 //BufferedReader
-/*
 
-public class q10807 {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = 0;
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=0;i<n;i++){
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int a = Integer.parseInt(br.readLine());
+
+        for(int j=0;j<n;j++){
+            if(array[j] == a){
+                count++;
+            }
+        }
+        bw.write(count +"\n");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
 }
 
- */
+
