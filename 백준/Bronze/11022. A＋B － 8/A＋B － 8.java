48776c5cd75e1f7b4b7f93911e2633dde7e7891e
Junseop Kim <125976420+junseopkim-dev@users.noreply.github.com>
2023-09-12 22:13:27 +0900

    [Bronze V] Title: A+B - 8, Time: 168 ms, Memory: 16424 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,46 @@
+//빠른 버전
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+        for(int i = 0;i<t;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write("Case #"+(i+1)+": "+ a + " + "+ b +" = "+ (a+b)+ "\n");
+        }
+        bw.close();
+    }
+}
+
+
+/*
+//느린 버전
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        int t = stdIn.nextInt();
+        for (int i=0; i<t; i++){
+            int a = stdIn.nextInt();
+            int b = stdIn.nextInt();
+            System.out.println("Case #"+(i+1)+": "+a+" + "+b+ " = " + (a+b));
+        }
+        stdIn.close();
+    }
+
+}
+
+*/
