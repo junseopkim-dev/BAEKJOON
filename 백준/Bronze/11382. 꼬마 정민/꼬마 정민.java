64cef260e8b4438fe06c1b6fded247d9070e81f7
Junseop Kim <125976420+junseopkim-dev@users.noreply.github.com>
2023-09-12 13:43:43 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        long a = stdIn.nextLong();
+        long b = stdIn.nextLong();
+        long c = stdIn.nextLong();
+
+        System.out.println(a+b+c);
+    }
+}
