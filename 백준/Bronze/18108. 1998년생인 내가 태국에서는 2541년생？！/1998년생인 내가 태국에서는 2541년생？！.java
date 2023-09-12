13859ae1d6305c3a6f8761d0d734c515f48f2707
Junseop Kim <125976420+junseopkim-dev@users.noreply.github.com>
2023-09-12 12:34:54 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        int budyear = stdIn.nextInt();
+        System.out.println(budyear-(2541-1998));
+    }
+}
