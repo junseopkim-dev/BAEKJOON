86c06dd1a02d1733acb2f7c248377f386a342539
Junseop Kim <125976420+junseopkim-dev@users.noreply.github.com>
2023-09-12 12:40:47 +0900

    [Bronze V] Title: 나머지, Time: 208 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        int a = stdIn.nextInt();
+        int b = stdIn.nextInt();
+        int c = stdIn.nextInt();
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+}
