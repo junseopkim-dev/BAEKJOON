4a703cd27e0c1588eb3916676f8192dc3359bed0
Junseop Kim <125976420+junseopkim-dev@users.noreply.github.com>
2023-09-12 12:30:49 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        String nickname = stdIn.nextLine();
+        System.out.println(nickname+"??!");
+    }
+}
