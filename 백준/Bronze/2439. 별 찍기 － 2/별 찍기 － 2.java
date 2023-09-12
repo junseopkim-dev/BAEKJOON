c9ac967ea09b619a423a2653fe8e5e8346542d01
Junseop Kim <125976420+junseopkim-dev@users.noreply.github.com>
2023-09-13 00:46:40 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,3 +1,35 @@
+//빠른 버전
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                bw.write(" ");
+            }
+            for(int k=1;k<=i;k++){
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        bw.close();
+    }
+}
+
+
+
+
+//느린 버전
+/*
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
@@ -14,3 +46,4 @@ public class Main {
         }
     }
 }
+*/
