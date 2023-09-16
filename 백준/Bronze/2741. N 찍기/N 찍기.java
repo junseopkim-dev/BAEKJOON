6cd1ea9e06c790ae1d3a03fbe546a2d40eec62bc
Junseop Kim <125976420+junseopkim-dev@users.noreply.github.com>
2023-09-17 03:16:59 +0900

    [Bronze V] Title: N 찍기, Time: 288 ms, Memory: 25752 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0;i<n;i++){
+            bw.write(String.valueOf(i+1)+"\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
