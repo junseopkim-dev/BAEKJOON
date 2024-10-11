ba659c8774e2d3eb21acc492d86def2f8f5252a1
Junseop Kim <125976420+junseopkim-dev@users.noreply.github.com>
2024-10-12 05:47:36 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int age) {
+        
+        int baseyear = 2022;
+        
+        
+        int birthyear = baseyear-age+1;
+        return birthyear;
+    }
+}
